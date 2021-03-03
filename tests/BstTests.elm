be9144e091de89c58d52a140cfe7773bcf6e1fd7
Colin Logue <email@colinlogue.me>
2021-03-03 00:45:21 -0800

    added fuzz tests for conversion to and from lists

--- tests/BstTests.elm
+++ tests/BstTests.elm
@@ -1,33 +1,13 @@
 module BstTests exposing (..)
 
 import Expect
-import Fuzz exposing (Fuzzer, int, tuple, custom)
+import Fuzz exposing (Fuzzer, int, tuple, custom, list)
 import Random exposing (Generator)
-import Shrink exposing (Shrinker, merge, noShrink)
+import Shrink exposing (Shrinker, noShrink)
 import Test exposing (..)
+import List.Extra exposing (unique)
 import BST exposing (..)
 
--- fuzzer to produce sorted BSTs
-sortedBST : Fuzzer BST
-sortedBST =
-  let
-    f x t = case t of
-      Leaf -> singleton x
-      Node n l r -> if x < n then Node n (f x l) r else if n < x then Node n l (f x r) else t
-
-    generator : Generator BST
-    generator = Random.map (List.foldl f empty) (Random.list 100 (Random.int -100 100))
-
-    shrinker : Shrinker BST
-    shrinker t =
-      case t of
-        Leaf -> noShrink Leaf
-        Node n l r ->
-          Shrink.map Node (noShrink n)
-            |> Shrink.andMap (shrinker l)
-            |> Shrink.andMap (shrinker r)
-  in
-    custom generator noShrink
 
 bstTests : Test
 bstTests =
@@ -59,6 +39,34 @@ bstTests =
     --    \_ -> Expect.equal x y
     
     ]
+
+
+-- The remaining tests are commented out because they are for the fromList
+-- and toList functions. Uncomment them when you start to work on those.
+
+{-  This is the start of a multi-line comment.
+    It lasts until the matching closing brace.
+
+listConversionTests : Test
+listConversionTests =
+  describe "Tests for converting between BSTs and lists"
+
+    [ fuzz (list int) "fromList produces sorted tree" <|
+      \xs ->
+        Expect.true "Expected the BST to be sorted"
+          <| isSorted (fromList xs)
+
+    , fuzz sortedBST "toList produces sorted list" <|
+      \t ->
+        Expect.equal (toList t) (List.sort (toList t))
+
+    , fuzz (list int) "toList << fromList is sort << unique" <|
+      \xs ->
+        Expect.equal (toList << fromList <| xs) (List.sort << unique <| xs)
+
+    ]
+
+-}
 
 -- fuzzer to produce sorted BSTs. ignore this for now.
 sortedBST : Fuzzer BST
